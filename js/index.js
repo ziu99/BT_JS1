10d7cfc191d1099e22a0e0d13b7cd7b1a789660b
Trieu <trieu186168@gmail.com>
2023-12-12 15:53:32 +0700

    new

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// ? CÂU 1
+
+
+let luong = 100000;
+let ngay = document.getElementById("day");
+
+let lanh = 0;
+lanh = luong * ngay;
+
+function submit(){
+    console.log("nhận được : "+ lanh );
+    let content = document.getElementById("result");
+}
+
+
+
